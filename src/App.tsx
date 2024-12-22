a8bb8492a43b75b384cfac62587a7b4a4cc488ec
Super Dev <super.dev.947@gmail.com>
2024-12-22 04:02:24 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div className='bg-blue-500 min-h-screen flex items-center justify-center'>
+      <h1 className='text-white text-4xl font-bold'>Welcome to React with Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
